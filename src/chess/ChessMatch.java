b87182e3db8ddeae7e31b4bd13429f19173f9f46
KelvimSaidel <kelvimsfs@gmail.com>
2021-06-30 23:44:50 -0300

    ChessException and ChessPosition with little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -27,9 +26,23 @@ public class ChessMatch {
 		
 	}	
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.PlacePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.PlacePiece(new Rook(board, Color.White), new Position(2, 1));
-	    board.PlacePiece(new King(board, Color.Black), new Position(0, 4));
-	    board.PlacePiece(new King(board, Color.White), new Position(7, 4));
+		placeNewPiece('c', 1, new Rook(board, Color.White));
+        placeNewPiece('c', 2, new Rook(board, Color.White));
+        placeNewPiece('d', 2, new Rook(board, Color.White));
+        placeNewPiece('e', 2, new Rook(board, Color.White));
+        placeNewPiece('e', 1, new Rook(board, Color.White));
+        placeNewPiece('d', 1, new King(board, Color.White));
+
+        placeNewPiece('c', 7, new Rook(board, Color.Black));
+        placeNewPiece('c', 8, new Rook(board, Color.Black));
+        placeNewPiece('d', 7, new Rook(board, Color.Black));
+        placeNewPiece('e', 7, new Rook(board, Color.Black));
+        placeNewPiece('e', 8, new Rook(board, Color.Black));
+        placeNewPiece('d', 8, new King(board, Color.Black));
 	}
 }
