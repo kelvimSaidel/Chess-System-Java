a4ff3c03bb604b7e6ffe0fd32fae19c62db436ab
KelvimSaidel <kelvimsfs@gmail.com>
2021-07-19 23:37:56 -0300

    Implementing possible moves of king

--- src/application/UI.java
+++ src/application/UI.java
@@ -82,7 +82,7 @@ public class UI {
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 		if (piece == null) {
-            System.out.print("-"+ANSI_RESET);
+            System.out.print("-"+ ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.White) {
